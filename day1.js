d4144de6d5f8cdb4282030a3ba0e20bd53c43540
kofi gordor <kofigordor@gmail.com>
2022-06-16 12:29:50 +0200

    new js created

---
+++ day1.js
@@ -0,0 +1,28 @@
+//Day 1
+//Part 1
+const paintColor = "blue";
+const make = "Toyota";
+const model = "Yaris";
+const year = "2009";
+const isManualTransmission = "true";
+const seats = 5;
+const mileage = 20000;
+const isElectric = "true";
+const drivePosition = "left-hand";
+
+console.log(
+  paintColor,
+  make,
+  model,
+  year,
+  isManualTransmission,
+  seats,
+  mileage,
+  isElectric,
+  drivePosition
+);
+
+//Part 2
+const str = "I will be a web developer by the end of the year";
+const newStr = str.split("");
+console.log(newStr.reverse().join(""));
